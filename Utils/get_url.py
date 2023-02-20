53c70fb539d0893a10a7db42ecca2f83d6549d97
coo11 <22176261+coo11@users.noreply.github.com>
2023-02-21 03:05:22 +0800

    Bug fix

--- Utils/get_url.py
+++ Utils/get_url.py
@@ -12,7 +12,7 @@ def post(arch, ap):
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <request protocol="3.0" version="1.3.23.9" shell_version="1.3.21.103" ismachine="0" sessionid="{{3597644B-2952-4F92-AE55-D315F45F80A5}}" installsource="ondemandcheckforupdate" requestid="{{CD7523AD-A40D-49F4-AEEF-8C114B804658}}" dedup="cr">
 	<hw sse="1" sse2="1" sse3="1" ssse3="1" sse41="1" sse42="1" avx="1" physmemory="12582912" />
-	<os platform="win" version="6.3" arch="{arch}" />
+	<os platform="win" version="10.0" arch="{arch}" />
 	<app appid="{{8A69D345-D564-463C-AFF1-A69D9E530F96}}" ap="{ap}">
 		<updatecheck />
 	</app>
